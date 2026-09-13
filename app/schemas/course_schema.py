49826f63420d322db9e6bda8043afcbed6223042
Indixpert-vish-2025 <vjaiss047@gmail.com>
2026-09-13 13:03:57 +0530

    Delete app directory

--- app/schemas/course_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Course(BaseModel):
-    name: str
-    teacher_id: int | None = None
--- app/schemas/student_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Student(BaseModel):
-    name: str
-    email: str
